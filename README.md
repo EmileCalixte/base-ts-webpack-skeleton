16c228a0433892872268d635838a014507f2c8ba
Emile Calixte <calixte.emile@gmail.com>
2022-03-24 21:26:11 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Base skeleton for JS app with webpack
+
+A very basic skeleton for a JavaScript app using webpack
+
+## Install dependencies
+
+```sh 
+npm install
+```
+## Build
+
+Webpack takes all files from `src/` and bundles them in `dist/`.
+
+### Watch for changes and rebuild automatically
+
+```sh
+npm run watch
+```
+
+### Create development builds
+
+```sh
+npm run dev
+```
+
+### Create production builds
+
+```sh
+npm run build
+```
+
+## Additional info
+
+### Include static images in HTML
+
+You can link any image in the `src/` directory in your HTML files with the following syntax:
+
+```
+<%require('./path/to/file.ext')%>
+```
+
+For example, assuming you have an image whose path is `src/img/image.png`, you can include it in your `index.html` as follows:
+
+```html
+<img src="<%=require('./img/image.png')%>" alt="My image">
+```
+
+The image will be copied into the `dist/` folder at build time.
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/js/index.js",
+    target: ["web", "es5"],
+    devtool: "source-map", // Generate map files
+    plugins: [
+        new HtmlWebpackPlugin({ // Generate an index.html file in build directory from the one in src directory
+            filename: "index.html",
+            template: "./src/index.html",
+        }),
+    ],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, // Delete old files before creating new ones
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
