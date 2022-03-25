34b05febcfb338e96a22a92daaf8bdf66b658570
Emile Calixte <calixte.emile@gmail.com>
2022-03-25 13:26:06 +0100

    Add ts-loader in webpack config to make webpack understand TS files

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
