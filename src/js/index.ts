637c9d11b0daac7507e42f87b33926ef749875f9
Emile Calixte <calixte.emile@gmail.com>
2022-03-25 13:31:29 +0100

    Check that d.ts files in build directory contain declarations and JSDoc

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,3 +1,21 @@
 import "../css/index.css";
 
-console.log("Hello world");
+/**
+ * Represents a user with a firstname and a lastname
+ */
+type User = {
+    firstname: string,
+    lastname: string,
+}
+
+/**
+ * Generates a new {@see User}
+ * @param {string} firstname The user's firstname
+ * @param {string} lastname The user's lastname
+ */
+export function getNewUser(firstname: string, lastname: string): User {
+    return {
+        firstname,
+        lastname
+    };
+}
